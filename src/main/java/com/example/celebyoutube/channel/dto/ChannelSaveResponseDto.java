4bd497af737fa0af117b6ce01b822a1a521dff2f
bactoria <bactoria@gmail.com>
2018-12-14 17:37:59 +0900

    불필요한 필드 제거 ( ChannelSaveResponseDto.class ) save 시 반환하는 channel 객체에는 애초에 위의 필드가 제공되지 않음.

--- src/main/java/com/example/celebyoutube/channel/dto/ChannelSaveResponseDto.java
+++ src/main/java/com/example/celebyoutube/channel/dto/ChannelSaveResponseDto.java
@@ -1,12 +1,10 @@
 package com.example.celebyoutube.channel.dto;
 
 import com.example.celebyoutube.channel.Channel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 @ToString
@@ -16,24 +14,10 @@ public class ChannelSaveResponseDto {
     private String id;
     private String name;
     private String introVideoUrl;
-    private String title;
-    private String content;
-    private String image;
-    private Long subscriber;
-    private LocalDate joinDate;
-    private Long views;
-    private LocalDateTime updatedTime;
 
     public ChannelSaveResponseDto(Channel channel) {
         this.id = channel.getId();
         this.name = channel.getName();
         this.introVideoUrl = channel.getIntroVideoUrl();
-        this.title = channel.getTitle();
-        this.content = channel.getContent();
-        this.image = channel.getImage();
-        this.subscriber = channel.getSubscriber();
-        this.joinDate = channel.getJoinDate();
-        this.views = channel.getViews();
-        this.updatedTime = channel.getUpdatedTime();
     }
 }
