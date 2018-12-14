243b7a300eace8713f5a70bde88cf86782410505
bactoria <bactoria@gmail.com>
2018-12-14 18:48:29 +0900

    Error나는 테스트코드 추가 issue #1

--- src/test/java/com/example/celebyoutube/channel/ChannelServiceTest.java
+++ src/test/java/com/example/celebyoutube/channel/ChannelServiceTest.java
@@ -29,5 +29,77 @@ public class ChannelServiceTest {
         assertThat(channelRepositoryMock).isNotNull();
         assertThat(channelService).isNotNull();
     }
-    
+
+    @Test
+    public void channel을_정상적으로_크롤링한다() throws IOException {
+        //given
+        final String ID = "UCCljTtZh6Rt1uxFH_NRkIEw";
+
+        final String TITLE = "유튜튜 테스트";
+        final String CONTENT = "백엔드 테스트용 채널입니다.";
+        final String IMAGE = "https://yt3.ggpht.com/a-/AN66SAx-2HMivOo50k0DBE7PmMGPLKxG3H4CLuXOCA=s900-mo-c-c0xffffffff-rj-k-no";
+        final LocalDate JOIN_DATE = LocalDate.of(2018, 12, 13);
+
+        //when
+        this.channelService.updateChannel(ID);
+
+        //then
+        verify(channelRepositoryMock).updateChannel(eq(ID), eq(TITLE), eq(CONTENT), eq(IMAGE), anyLong(), eq(JOIN_DATE), anyLong(), any());
+    }
+
+    @Test
+    public void channel을_정상적으로_추가한다() throws IOException {
+        //given
+        final String ID = "UCCljTtZh6Rt1uxFH_NRkIEw";
+        final String NAME = "NAME";
+        final String INTRO_VIDEO_URL = "INTRO_VIDEO_URL";
+
+        Channel channel = ChannelSaveRequestDto.builder().id(ID).name(NAME)
+                .introVideoUrl(INTRO_VIDEO_URL).build()
+                .toEntity();
+
+        given(channelRepositoryMock.save(any(Channel.class))).willReturn(channel);
+
+        ChannelSaveRequestDto requestDto = ChannelSaveRequestDto.builder()
+                .id(ID)
+                .name(NAME)
+                .introVideoUrl(INTRO_VIDEO_URL)
+                .build();
+
+        //when
+        ChannelSaveResponseDto result = this.channelService.saveChannel(requestDto);
+
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(ID);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getIntroVideoUrl()).isEqualTo(INTRO_VIDEO_URL);
+    }
+
+    @Test
+    public void channel을_추가할때_관련정보를_크롤링하여_갱신한다() throws IOException {
+        //given
+        final String ID = "UCCljTtZh6Rt1uxFH_NRkIEw";
+        final String NAME = "NAME";
+        final String INTRO_VIDEO_URL = "INTRO_VIDEO_URL";
+
+        Channel channel = ChannelSaveRequestDto.builder().id(ID).name(NAME)
+                .introVideoUrl(INTRO_VIDEO_URL).build()
+                .toEntity();
+
+        given(channelRepositoryMock.save(any(Channel.class))).willReturn(channel);
+
+        ChannelSaveRequestDto requestDto = ChannelSaveRequestDto.builder()
+                .id(ID)
+                .name(NAME)
+                .introVideoUrl(INTRO_VIDEO_URL)
+                .build();
+
+        //when
+        this.channelService.saveChannel(requestDto);
+
+        //then
+        verify(channelRepositoryMock).updateChannel(eq(ID), any(), any(), any(), any(), any(), any(), any());
+    }
+
 }
